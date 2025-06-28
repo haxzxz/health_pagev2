ef248bab1002d15951b9eb3ce36ebe479fbc04f7
Christian <howuselessislife@gmail.com>
2025-06-28 17:13:58 +0800

    Update page from MAIN

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// For initializing Header
-function initializeHeaderScript() {
-    const header = document.querySelector('#header');
-    const menu = document.querySelector('#menu');
-    const close = document.querySelector('#close');
-    const nav = document.querySelector('#nav');
-    const body = document.querySelector('body');
-    const headerHeight = header.getBoundingClientRect().height;
-
-
-    menu.addEventListener('click', () => {
-        nav.classList.add('open-nav');
-    })
-
-    close.addEventListener('click', () => {
-        nav.classList.remove('open-nav');
-    })
-
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > headerHeight) {
-            header.classList.add('sticky');
-        } else {
-            header.classList.remove('sticky');
-            header.classList.remove('animate-fadein-delayed');
-            header.classList.remove('hidden');
-        }
-    });
-}
-
 // Script for Hovering Cards
 function setupHoverCard(cardId, summaryHTML, detailHTML) {
     const card = document.getElementById(cardId);
@@ -555,15 +526,3 @@ setupPartialToggle("solid-button", "toggle-section", {
         </div>
   `
 });
-
-/* footer */
-function initializeFooterScript() {
-    const currentYearElement = document.querySelector('#currentYearElement');
-    const date = new Date();
-    const currentYear = date.getFullYear();
-
-    currentYearElement.textContent = currentYear;
-}
-
-initializeHeaderScript();
-initializeFooterScript();
